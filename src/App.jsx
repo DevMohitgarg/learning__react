d43d47950d3ae6877742025ba8d7937c6dbebff5
Mohit Kumar Garg <mohitkumargarg20@gmail.com>
2024-11-22 13:10:48 +0530

    starting with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+     <div>
+        <h1 className=" p-[300px]  bg-red-400  text-8xl font-bold text-white text-center ">
+          Blog Website
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
